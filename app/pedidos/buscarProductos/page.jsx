068910f599efa34f6a39bf4b2aa1878a4e42484d
jaime urrego <or4846@gmail.com>
2023-09-20 21:28:03 -0500

    se crea pagina buscar Productos

---
+++ app/pedidos/buscarProductos/page.jsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import styles from 'app/page.module.css'
+import FormPedidosProductos from '@/componentes/FormpedidosProductos'
+import NavbarBotones from '@/componentes/NavbarBotones'
+import Navbar from '@/componentes/Navbar'
+import Flooter from '@/componentes/Flooter'
+
+
+
+export default function Home() {
+  return (
+    
+    <main className={styles.main}>
+    
+      <Navbar/>   
+
+      <div className={styles.container}>
+        
+      <div className={styles.navmenu}>
+        <NavbarBotones />
+    
+      </div>
+    
+      <div className={styles.ventaderecha}>
+      <FormPedidosProductos />
+ 
+      </div>
+      
+        </div>
+        
+      
+<div className={styles.flooter}>
+<Flooter/>
+</div>
+    </main>
+  )
+}
